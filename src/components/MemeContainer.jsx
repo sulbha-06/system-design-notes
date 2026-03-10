2fb8f602b7c36ae9d4db3c00370c29769e5be24c
Sulbha-06 <sulbha06@gmail.com>
2026-03-10 22:24:16 +0530

    Refactor MemeContainer to use async/await for fetching memes and implement infinite scroll

--- src/components/MemeContainer.jsx
+++ src/components/MemeContainer.jsx
@@ -4,24 +4,42 @@ import "./cards.css";
 import ShimmerCards from "./ShimmerCards";
 function MemeContainer() {
   const [memeList, setMemeList] = React.useState([]);
+  const [showShimmer, setshowShimmer] = React.useState(true);
+  const fetchMemes = async () => {
+    try {
+      setshowShimmer(true);
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setMemeList((memeList) => [...memeList, ...data.data.memes]);
+      setshowShimmer(false);
+    } catch (error) {
+      console.error("Error fetching memes:", error);
+    }
+  };
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setMemeList(data.data.memes))
-      .catch((error) => console.error("Error fetching memes:", error));
+    fetchMemes();
     console.log("MemeContainer mounted");
+
+    //infinite scroll
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+  const handleScroll = () => {
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+      console.log("Reached bottom of the page, fetching more memes...");
+      fetchMemes();
+    }
+  };
   return (
     <>
-      {" "}
       <h1>Meme Container</h1>
       <p>This is a meme container component.</p>
       <div className="meme-container">
-        {memeList.length ? (
-          memeList.map((meme) => <Cards key={meme.id} meme={meme} />)
-        ) : (
-          <ShimmerCards />
-        )}
+        {memeList.length
+          ? memeList.map((meme) => <Cards key={meme.id} meme={meme} />)
+          : showShimmer && <ShimmerCards />}
       </div>
     </>
   );
